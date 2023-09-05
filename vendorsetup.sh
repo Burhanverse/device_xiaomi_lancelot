9bf524e9e2c2025eafe8ff8adb90ca676625b3ea
burhanverse@gmail.com <burhanverse@gmail.com>
2023-09-05 20:54:35 +0530

    lancelot: switch to upstreamed kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ git clone --quiet https://github.com/Xiaomi-MT6768-Dev/prebuilt-apps --depth 1 x
 # vendor
 git clone --quiet https://github.com/Xiaomi-MT6768-Dev/proprietary_vendor_xiaomi --depth 1 vendor/xiaomi > /dev/null
 # kernel
-git clone --quiet https://github.com/nullptr03/kernel_xiaomi_mt6768 -b kangboi --depth 1 kernel/xiaomi/mt6768 > /dev/null
+git clone https://github.com/Xiaomi-MT6768-Dev/kernel_xiaomi_mt6768 --depth 1 kernel/xiaomi/mt6768 > /dev/null
 # mtk sepolicy vendor
 rm -rf device/mediatek/sepolicy_vndr
 git clone --quiet https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr --depth 1 device/mediatek/sepolicy_vndr > /dev/null
