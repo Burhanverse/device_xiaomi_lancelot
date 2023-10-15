ceded9df41d7e5233fd07991b8d84ba8d489cc31
Burhanverse <burhanverse@gmail.com>
2023-10-15 23:44:29 +0530

    vendorsetup: cleanup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,10 +10,8 @@ git clone --depth 1 https://github.com/Xiaomi-MT6768-Dev/vendor_xiaomi_lancelot 
 # kernel
 git clone --depth 1 https://gitlab.com/MT6768Lab/KernelTree kernel/xiaomi/mt6768
 # mtk sepolicy vendor
-rm -rf device/mediatek/sepolicy_vndr
 git clone --depth 1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b 14 device/mediatek/sepolicy_vndr
 # mtk hardware 
-rm -rf hardware/mediatek
 git clone --depth 1 https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek
 # clang
 git clone --depth 1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r487747c prebuilts/clang/host/linux-x86/clang-r487747c
