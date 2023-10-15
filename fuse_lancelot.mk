b324d53f0cb1c43be47790b52c8473a98bf0f64e
Burhanverse <burhanverse@gmail.com>
2023-10-15 22:34:20 +0530

    lancelot: fixup

--- fuse_lancelot.mk
+++ fuse_lancelot.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/fuse/config/common_full_phone.mk)
+$(call inherit-product, vendor/fuse/config/common.mk)
 
 
 # Device identifier. This must come after all inclusions.
