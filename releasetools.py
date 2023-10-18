ce925369199b01743546090bc6cc85c0523a3a26
Burhanverse <burhanverse@gmail.com>
2023-10-18 17:32:59 +0530

    lancelot: Include MIUI V12.5.8.0.RJCCNXM firmware [2/2] - commit [1/2] 44ed8aa35ab7ec35f5062aeccb4cdf05abd823e1
    Signed-off-by: Burhanverse <burhanverse@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -1,19 +1,3 @@
-#
-# Copyright (C) 2022 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 import common
 import re
 
@@ -25,16 +9,161 @@ def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
   return
 
-def AddImage(info, basename, dest):
-  name = basename
-  data = info.input_zip.read("IMAGES/" + basename)
-  common.ZipWriteStr(info.output_zip, name, data)
+def AddImage(info, name, dest, firmware):
+  AddImageToZIP(info, name, firmware)
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
+def AddImageToZIP(info, name, firmware):
+  if firmware:
+    data = info.input_zip.read("RADIO/" + name)
+  else:
+    data = info.input_zip.read("IMAGES/" + name)
+  common.ZipWriteStr(info.output_zip, name, data)
+
+def PackageExtractFile(info, name, dest):
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
+def PackageExtractFile_Preloader(info, name, dest, is_emmc):
+  if is_emmc:
+    info.script.AppendExtra("""assert(set_emmc_writable("/sys/block/%s/force_ro"),
+        package_extract_file("%s", "/dev/block/%s"));""" % (dest, name, dest))
+  else:
+    info.script.AppendExtra('assert(package_extract_file("%s", "/dev/block/%s"));' % (name, dest))
+
+def SwitchActive(info, a, b):
+  info.script.AppendExtra('switch_active("%s", "%s");' % (a, b))
+
+def ShowMTUpdateStage(info):
+  info.script.AppendExtra('show_mtupdate_stage("/cache/recovery/last_mtupdate_stage");')
+
+def SetMTUpdateStage(info, value):
+  info.script.AppendExtra('set_mtupdate_stage("/cache/recovery/last_mtupdate_stage", "%s");' % value)
+
+def DeleteMTUpdateStage(info):
+  info.script.AppendExtra('delete("/cache/recovery/last_mtupdate_stage");')
+
 def OTA_InstallEnd(info):
-  info.script.Print("Patching firmware images...")
-  AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
-  AddImage(info, "vbmeta_system.img", "/dev/block/by-name/vbmeta_system")
-  AddImage(info, "vbmeta_vendor.img", "/dev/block/by-name/vbmeta_vendor")
-  AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
+  AddImageToZIP(info, "lk.img", True)
+  AddImageToZIP(info, "md1img.img", True)
+  AddImageToZIP(info, "preloader_emmc.img", True)
+  AddImageToZIP(info, "preloader_raw.img", True)
+  AddImageToZIP(info, "preloader_ufs.img", True)
+  AddImageToZIP(info, "scp.img", True)
+  AddImageToZIP(info, "spmfw.img", True)
+  AddImageToZIP(info, "sspm.img", True)
+  AddImageToZIP(info, "tee.img", True)
+
+  Firmware_Images(info)
+  return
+
+def Firmware_Images(info):
+
+  ShowMTUpdateStage(info)
+
+  info.script.AppendExtra('ifelse (')
+  info.script.AppendExtra('less_than_int(get_mtupdate_stage("/cache/recovery/last_mtupdate_stage"), "1") ,')
+  info.script.AppendExtra('(')
+
+  info.script.Print('start to update general image')
+
+  AddImage(info, "dtbo.img", "/dev/block/platform/bootdevice/by-name/dtbo", False)
+  # logo.bin
+  PackageExtractFile(info, "md1img.img", "/dev/block/platform/bootdevice/by-name/md1img")
+  PackageExtractFile(info, "spmfw.img", "/dev/block/platform/bootdevice/by-name/spmfw")
+
+  SetMTUpdateStage(info, "1")
+
+  info.script.AppendExtra('),')
+
+  info.script.Print('general images are already updated')
+
+  info.script.AppendExtra(');')
+
+  info.script.AppendExtra('ifelse (')
+  info.script.AppendExtra('less_than_int(get_mtupdate_stage("/cache/recovery/last_mtupdate_stage"), "3") ,')
+  info.script.AppendExtra('(')
+  info.script.AppendExtra('if less_than_int(get_mtupdate_stage("/cache/recovery/last_mtupdate_stage"), "2") then')
+
+  info.script.Print('start to update alt loader image')
+
+  PackageExtractFile(info, "sspm.img", "/dev/block/platform/bootdevice/by-name/sspm_2")
+  PackageExtractFile(info, "tee.img", "/dev/block/platform/bootdevice/by-name/tee2")
+  PackageExtractFile(info, "scp.img", "/dev/block/platform/bootdevice/by-name/scp2")
+  PackageExtractFile(info, "lk.img", "/dev/block/platform/bootdevice/by-name/lk2")
+
+  info.script.AppendExtra('if get_storage_type() then')
+
+  PackageExtractFile_Preloader(info, "preloader_ufs.img", "mmcblk0boot1", False)
+
+  info.script.AppendExtra('else')
+  
+  PackageExtractFile_Preloader(info, "preloader_emmc.img", "mmcblk0boot1", True)
+  
+  info.script.AppendExtra('endif;')
+
+  SetMTUpdateStage(info, "2")
+
+  info.script.AppendExtra('endif;')
+
+  info.script.AppendExtra('')
+
+  SwitchActive(info, "sspm_1", "sspm_2")
+  SwitchActive(info, "tee1", "tee2")
+  SwitchActive(info, "scp1", "scp2")
+  SwitchActive(info, "lk", "lk2")
+  SwitchActive(info, "preloader", "preloader2")
+
+  SetMTUpdateStage(info, "3")
+
+  info.script.AppendExtra('),')
+
+  info.script.Print('alt loader images are already updated')
+
+  info.script.AppendExtra(');')
+
+  info.script.AppendExtra('ifelse (')
+  info.script.AppendExtra('less_than_int(get_mtupdate_stage("/cache/recovery/last_mtupdate_stage"), "5") ,')
+  info.script.AppendExtra('(')
+  info.script.AppendExtra('if less_than_int(get_mtupdate_stage("/cache/recovery/last_mtupdate_stage"), "4") then')
+
+  info.script.Print('start to update main loader image')
+
+  PackageExtractFile(info, "sspm.img", "/dev/block/platform/bootdevice/by-name/sspm_1")
+  PackageExtractFile(info, "tee.img", "/dev/block/platform/bootdevice/by-name/tee1")
+  PackageExtractFile(info, "scp.img", "/dev/block/platform/bootdevice/by-name/scp1")
+  PackageExtractFile(info, "lk.img", "/dev/block/platform/bootdevice/by-name/lk")
+
+  info.script.AppendExtra('if get_storage_type() then')
+
+  PackageExtractFile_Preloader(info, "preloader_ufs.img", "mmcblk0boot0", False)
+
+  info.script.AppendExtra('else')
+  
+  PackageExtractFile_Preloader(info, "preloader_emmc.img", "mmcblk0boot0", True)
+  
+  info.script.AppendExtra('endif;')
+
+  SetMTUpdateStage(info, "4")
+
+  info.script.AppendExtra('endif;')
+
+  info.script.AppendExtra('')
+
+  SwitchActive(info, "sspm_2", "sspm_1")
+  SwitchActive(info, "tee2", "tee1")
+  SwitchActive(info, "scp2", "scp1")
+  SwitchActive(info, "lk2", "lk")
+  SwitchActive(info, "preloader2", "preloader")
+
+  info.script.AppendExtra('),')
+
+  info.script.Print('main loader images are already updated')
+
+  info.script.AppendExtra(');')
+
+  DeleteMTUpdateStage(info)
+
+  AddImage(info, "vbmeta_system.img", "/dev/block/platform/bootdevice/by-name/vbmeta_system", False)
+  AddImage(info, "vbmeta.img", "/dev/block/platform/bootdevice/by-name/vbmeta", False)
+  AddImage(info, "vbmeta_vendor.img", "/dev/block/platform/bootdevice/by-name/vbmeta_vendor", False)
   return
