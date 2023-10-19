9398ed23b02b3d0f1fb112c90857cf892343976d
Burhanverse <burhanverse@gmail.com>
2023-10-19 19:48:50 +0530

    lancelot: update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,14 +3,12 @@ end="\033[0m"
 
 # Clone dependencies
 echo -e "${color}Cloning dependencies...${end}"
-# firmware
-git clone --depth 1 https://github.com/Xiaomi-MT6768-Dev/device_xiaomi_lancelot-firmware device/xiaomi/lancelot-firmware
 # vendor
 git clone --depth 1 https://github.com/Xiaomi-MT6768-Dev/vendor_xiaomi_lancelot -b 14 vendor/xiaomi/lancelot
 # kernel
 git clone --depth 1 https://gitlab.com/MT6768Lab/KernelTree kernel/xiaomi/mt6768
 # mtk sepolicy vendor
-git clone --depth 1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b 14 device/mediatek/sepolicy_vndr
+git clone --depth 1 https://github.com/Xiaomi-MT6768-Dev/device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 # mtk hardware 
 git clone --depth 1 https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek
 # clang
